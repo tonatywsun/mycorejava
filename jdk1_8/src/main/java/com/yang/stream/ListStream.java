552c87d6df36812a099327c346f0fb6c12ce290d
tonasun <tona.sun@meetsocial.cn>
2021-02-14 22:49:27 +0800

    stream

--- jdk1_8/src/main/java/com/yang/stream/ListStream.java
+++ jdk1_8/src/main/java/com/yang/stream/ListStream.java
@@ -1,11 +1,14 @@
 package com.yang.stream;
 
 import com.google.common.collect.Lists;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -23,23 +26,49 @@ public class ListStream {
         System.out.println(stream);
     }
 
-    public void flatMapToLong(Long tagId) {
+    public List<Long> flatMapToLong(Long tagId) {
         List<String> blacklistList = Lists.newArrayList("1,2,3");
         //判断这个tagID是不是 null
-        List<Long> longs = Optional.ofNullable(tagId)
-                                   //直接装到list中去
-                                   .map(Lists::newArrayList)
-                                   .orElse(blacklistList.stream()
-                                                        //先切开
-                                                        .map(u -> Arrays.asList(u.trim().split(",")))
-                                                        //转成long
-                                                        .flatMapToLong(x -> x.stream().mapToLong(Long::valueOf))
-                                                        //去重
-                                                        .distinct()
-                                                        //装到list中去
-                                                        .collect(Lists::newArrayList,
-                                                                 ArrayList::add,
-                                                                 List::addAll));
+        List<Long> tagIdList = Optional.ofNullable(tagId)
+                                       //直接装到list中去
+                                       .map(Lists::newArrayList)
+                                       //据说parallelStream比stream更快
+                                       .orElse(blacklistList.parallelStream()
+                                                            //先切开
+                                                            .map(u -> Arrays.asList(u.trim().split(",")))
+                                                            //转成long
+                                                            .flatMapToLong(x -> x.stream().mapToLong(Long::valueOf))
+                                                            //去重
+                                                            .distinct()
+                                                            //装到list中去
+                                                            .collect(Lists::newArrayList,
+                                                                     ArrayList::add,
+                                                                     List::addAll));
+        System.out.println(tagIdList);
+        return tagIdList;
+    }
 
+    @Test
+    public  void anyMatchTest() {
+        List<Integer> list = Arrays.asList(7, 6, 9, 3, 8, 2, 1);
+        // 遍历输出符合条件的元素
+        list.stream().filter(x -> x > 6).forEach(System.out::println);
+        // 匹配第一个
+        Optional<Integer> findFirst = list.stream().filter(x -> x > 6).findFirst();
+        // 匹配任意（适用于并行流）
+        Optional<Integer> findAny = list.parallelStream().filter(x -> x > 6).findAny();
+        // 是否包含符合特定条件的元素
+        boolean anyMatch = list.stream().anyMatch(x -> x < 6);
+        System.out.println("匹配第一个值：" + findFirst.get());
+        System.out.println("匹配任意一个值：" + findAny.get());
+        System.out.println("是否存在大于6的值：" + anyMatch);
+    }
+
+    @Test
+    public  void partitioningByTest() {
+        List<Integer> list = Arrays.asList(7, 6, 9, 3, 8, 2, 1);
+        //将list按x > 6条件分组
+        Map<Boolean, List<Integer>> part = list.stream().collect(Collectors.partitioningBy(x -> x > 6));
+        System.out.println(part);
     }
 }
